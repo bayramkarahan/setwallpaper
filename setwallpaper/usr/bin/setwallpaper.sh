0a685a5eee65c0ce095b6f86304dba17a8b786e0
bayram <bayramk@gmail.com>
2025-04-13 21:37:03 +0300

    test

--- setwallpaper/usr/bin/setwallpaper.sh
+++ setwallpaper/usr/bin/setwallpaper.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 wallpaperstatus="$HOME/.config/wp"
-
+feh --bg-tile /usr/share/images/duvar.svg
+exit 0
 if [ -f $wallpaperstatus ]; then
 	echo "dosya var"
+	feh --bg-tile /usr/share/images/duvar.svg
 else
 	echo "dosya yok"
 	touch $wallpaperstatus
-	gsettings set org.gnome.desktop.background picture-uri file:///usr/share/images/duvar.svg	
+	#gsettings set org.gnome.desktop.background picture-uri file:///usr/share/images/duvar.svg
+	feh --bg-tile /usr/share/images/duvar.svg
 fi
